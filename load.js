366ecd37450c50e2f7e87263e0f3369dc340eb25
troyosaurusrex <twb2593@yahoo.com>
2022-06-23 15:16:32 -0500

    Persistent header
    Added header to all content pages

--- load.js
+++ load.js
@@ -1,4 +1,5 @@
 $(function(){
       $("#left-sidebar").load("https://the64thsanctum.neocities.org/Sidebar.html");
     $("#navbar").load("https://the64thsanctum.neocities.org/navbar.html"); 
+    $("#header").load("https://the64thsanctum.neocities.org/header.html"); 
 });
