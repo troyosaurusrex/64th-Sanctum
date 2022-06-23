326a70509dc49326c22588082be1c4b7970281e9
troyosaurusrex <twb2593@yahoo.com>
2022-06-23 14:46:02 -0500

    Persistent navbar
    Adding navbar to all existing Contentpages

--- load.js
+++ load.js
@@ -1,3 +1,4 @@
 $(function(){
-      $("#left-sidebar").load("https://the64thsanctum.neocities.org/Sidebar.html"); 
+      $("#left-sidebar").load("https://the64thsanctum.neocities.org/Sidebar.html");
+    $("#navbar").load("https://the64thsanctum.neocities.org/navbar.html"); 
 });
