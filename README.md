1b1bc3ae64114478a514fcb54b2994d928d067a1
troyosaurusrex <twb2593@yahoo.com>
2022-07-07 18:07:32 -0500

    Revamp

--- README.md
+++
@@ -1,1 +0,0 @@
-This is my first website, created on Neocities. It's partially a blog, partially a sandbox while I learn HTML/CSS/Javascript.
--- load.js
+++ load.js
@@ -1,5 +1,7 @@
 $(function(){
-      $("#left-sidebar").load("https://the64thsanctum.neocities.org/Sidebar.html");
-    $("#navbar").load("https://the64thsanctum.neocities.org/navbar.html"); 
-    $("#header").load("https://the64thsanctum.neocities.org/header.html"); 
+    $("#left-sidebar").load("https://the64thsanctum.net/Static/LeftSidebar.html");
+    $("#right-sidebar").load("https://the64thsanctum.net/Static/RightSidebar.html");
+    $("#navbar").load("https://the64thsanctum.net/Static/navbar.html"); 
+    $("#header").load("https://the64thsanctum.net/Static/header.html"); 
+    $("#footer").load("https://the64thsanctum.net/Static/footer.html"); 
 });
